3397c295def96cf77cfe7dd3dbdf1ec1c138132d
MyChadum07 <156446728+MyChadum07@users.noreply.github.com>
2024-03-26 20:31:20 +0700

    ก็ใกล้ละล่ะ

--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -10,7 +10,7 @@ export default function BookingList () {
     if (bookItem.length == 0) return (
         <div className="p-10 flex flex-row justify-center">
             <div className="p-3 font-serif text-4xl bg-white text-sky-900 rounded-2xl">
-                No Vaccine Booking
+                No Campground Booking
             </div>
         </div>
     )
@@ -22,7 +22,7 @@ export default function BookingList () {
                     <div className="bg-white rounded-2xl px-5 mx-5 py-5 my-2 text-black space-y-1 font-serif" key={bookingItem.id}>
                         <div className="text-xl">{bookingItem.name} {bookingItem.surname}</div>
                         <div className="text-sm">Citizen ID : {bookingItem.id}</div>
-                        <div className="text-sm">Hospital : {bookingItem.hospital}</div>
+                        <div className="text-sm">Campground : {bookingItem.campground}</div>
                         <div className="text-md">Booking Date : {bookingItem.bookDate}</div>
                         <button className="block rounded-md bg-sky-600 hover:bg-sky-900 px-3 py-2 text-white shadow-sm" onClick={() => dispatch(removeBooking(bookingItem.id))}>
                             Remove Booking
